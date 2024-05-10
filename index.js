b66ea7ac609318d6385b0d83207047089d59b018
taolabomay11 <animator09nam1311@outlook.com>
2024-05-10 13:43:52 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
-  }
-  return arr;
+function getMin(arr) {
+  return Math.min(...arr);
 }
