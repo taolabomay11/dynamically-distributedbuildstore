aa4f63aa5366b81fe30f2df39c30a0ccebb421b5
taolabomay11 <animator09nam1311@outlook.com>
2024-05-15 07:35:11 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-function ladderLength(beginWord, endWord, wordList) {
-  const wordSet = new Set(wordList);
-  if (!wordSet.has(endWord)) return 0;
-  let count = 0;
-  const queue = [beginWord];
-  while (queue.length) {
-    const size = queue.length;
-    count++;
-    for (let i = 0; i < size; i++) {
-      const current = queue.shift();
-      if (current === endWord) return count;
-      for (let j = 0; j < current.length; j++) {
-        for (let k = 97; k <= 122; k++) {
-          const newWord =
-            current.slice(0, j) + String.fromCharCode(k) + current.slice(j + 1);
-          if (wordSet.has(newWord)) {
-            queue.push(newWord);
-            wordSet.delete(newWord);
-          }
-        }
-      }
-    }
-  }
-  return 0;
+function countWords(str) {
+  return str.split(" ").filter((word) => word !== "").length;
 }
